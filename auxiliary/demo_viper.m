38830a4dae82a0b9ee0c851e74545e44a2c75976
pratesufop <pratesufop@users.noreply.github.com>
2016-11-23 16:11:02 -0200

    Update demo_viper.m

--- auxiliary/demo_viper.m
+++ auxiliary/demo_viper.m
@@ -1,3 +1,5 @@
 % Raphael Prates
+% Code implementing the work : Kernel Cross-View Collaborative Representation based Classification for Person Re-Identification
+% Arxiv 2016 -  https://arxiv.org/pdf/1611.06969v1.pdf
 setup_viper 
 KernelXCRC_VIPER

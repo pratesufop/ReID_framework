002835020c3a03a20b48d4010847a36aa2b3911c
pratesufop <pratesufop@users.noreply.github.com>
2016-11-23 16:10:37 -0200

    Update demo_cuhk01.m

--- auxiliary/demo_cuhk01.m
+++ auxiliary/demo_cuhk01.m
@@ -1,3 +1,5 @@
+% Code implementing the work : Kernel Cross-View Collaborative Representation based Classification for Person Re-Identification
+% Arxiv 2016 -  https://arxiv.org/pdf/1611.06969v1.pdf
 % Raphael Prates
 setup_cuhk01
 KernelXCRC_CUHK01
